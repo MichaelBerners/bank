062a493ddfeb3ae54f8a02b238e5f5e148a9885c
mihail <mihail_belonogov@bk.ru>
2022-02-22 17:56:02 +0300

    update

--- src/main/java/ru/belonogov/bank/domain/TransactionResponse.java
+++ src/main/java/ru/belonogov/bank/domain/TransactionResponse.java
@@ -1,11 +1,6 @@
 package ru.belonogov.bank.domain;
 
 import lombok.Data;
-import ru.belonogov.bank.domain.entity.Account;
-
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import java.sql.Timestamp;
 
 @Data
